9b6b6d271bee8ba890d55d67fc0cc82310e5462b
ryskit <ryosukekida.71@gmail.com>
2021-02-12 20:53:34 +0900

    done

--- abc/088/b.cpp
+++ abc/088/b.cpp
@@ -1,23 +1,24 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int N;
-    int a[110];  // 最大 100 個ですが余裕をもたせます
     cin >> N;
-    for (int i = 0; i < N; ++i) cin >> a[i];
 
-    sort(a, a + N, greater<int>());  // a[0:N] を大きい順にソート
-    int Alice = 0;
-    int Bob = 0;
-    for (int i = 0; i < N; ++i) {
-        if (i % 2 == 0) { // Alice のターン
-            Alice += a[i];
-        }
-        else { // Bob のターン
-            Bob += a[i];
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        cin >> A.at(i);
+    }
+    sort(A.begin(), A.end(), greater<int>());
+
+    int Alice = 0, Bob = 0;
+    for (int i = 0; i < A.size(); i++) {
+        if (i % 2 == 0) {
+            Alice += A.at(i);
+        } else {
+            Bob += A.at(i);
         }
     }
+
     cout << Alice - Bob << endl;
 }
