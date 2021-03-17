477264304eaf1bca0ba0f80ec2cce730c0d058a7
ryskit <ryosukekida.71@gmail.com>
2021-03-17 22:02:32 +0900

    add 088 c

--- abc/088/c.cpp
+++ abc/088/c.cpp
@@ -2,5 +2,36 @@
 using namespace std;
 
 int main() {
-    vector
+    vector<vector<int>>C(3, vector<int>(3));
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            int c;
+            cin >> c;
+            C[i][j] = c;
+        }
+    }
+
+    bool Result = true;
+
+    if (C[0][0] - C[0][1] != C[1][0] - C[1][1] || C[1][0] - C[1][1] != C[2][0] - C[2][1]) {
+        Result = false;
+    }
+
+    if (C[0][1] - C[0][2] != C[1][1] - C[1][2] || C[1][1] - C[1][2] != C[2][1] - C[2][2]) {
+        Result = false;
+    }
+
+    if (C[0][0] - C[1][0] != C[0][1] - C[1][1] || C[0][1] - C[1][1] != C[0][2] - C[1][2]) {
+        Result = false;
+    }
+
+    if (C[1][0] - C[2][0] != C[1][1] - C[2][1] || C[1][1] - C[2][1] != C[1][2] - C[2][2]) {
+        Result = false;
+    }
+
+    if (Result) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 }
