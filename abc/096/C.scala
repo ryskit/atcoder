0eeb1ee15ae145ab2f68a2214f3192e147a9e59f
ryskit <ryosukekida.71@gmail.com>
2021-09-18 17:41:39 +0900

    done abc095 - C

---
+++ abc/096/C.scala
@@ -0,0 +1,19 @@
+package C096
+
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val a, b, c, x, y = sc.nextInt
+  val answer = (0 to math.max(x, y) * 2).foldLeft(Int.MaxValue) {
+    case (min, ab) =>
+      val xx = (x - ab / 2).ceil.toInt match {
+        case tx if tx < 0 => 0
+        case tx           => tx
+      }
+      val yy = (y - ab / 2).ceil.toInt match {
+        case ty if ty < 0 => 0
+        case ty           => ty
+      }
+      min.min(xx * a + yy * b + c * ab)
+  }
+  println(answer)
+}
