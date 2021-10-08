992f55a01fb08697c64699d031e9dd28898bcc62
ryskit <ryosukekida.71@gmail.com>
2021-10-08 22:08:58 +0900

    done abc066 - B

---
+++ abc/066/B.scala
@@ -0,0 +1,19 @@
+package B066
+
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val s = sc.next
+  val (answer, _) = (2 to s.length - 2 by 2).foldLeft(0, s) {
+    case ((maxLength, prev), _) =>
+      val current = prev.dropRight(2)
+      val len = current.length
+      val (f, b) = current.splitAt(len / 2)
+      println(current, f, b)
+      if (f == b) {
+        (maxLength.max(current.length), current)
+      } else {
+        (maxLength, current)
+      }
+  }
+  println(answer)
+}
