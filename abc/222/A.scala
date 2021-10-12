839f004b7595c004f207ab710984466518bd6a80
ryskit <ryosukekida.71@gmail.com>
2021-10-12 20:34:16 +0900

    done abc222 - A

---
+++ abc/222/A.scala
@@ -0,0 +1,7 @@
+package A222
+
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val n = sc.next
+  println("0" * (4 - n.length) + n)
+}
--- build.sbt
+++ build.sbt
@@ -2,6 +2,6 @@ name := "atcoder"
 
 version := "0.1"
 
-scalaVersion := "2.13.6"
+scalaVersion := "2.13.1"
 
 scalacOptions ++= Seq("-language:higherKinds")
