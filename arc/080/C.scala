2368416bd92a730977c50454b329df90db6505a3
ryskit <ryosukekida.71@gmail.com>
2021-10-27 17:23:22 +0900

    done arc080 - C

---
+++ arc/080/C.scala
@@ -0,0 +1,22 @@
+package arc080C
+
+import scala.util.control.Breaks
+
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val n = sc.nextInt
+  var as = Vector.fill(n)(sc.nextLong)
+  val (m4, m2, mo) = as.foldLeft(0, 0, 0) {
+    case ((m4, m2, mo), a) =>
+      if (a % 4 == 0) (m4 + 1, m2, mo)
+      else if (a % 2 == 0) (m4, m2 + 1, mo)
+      else (m4, m2, mo + 1)
+  }
+
+  val answer =
+    if (m4 + 1 == mo && n == (m4 + mo)) "Yes"
+    else if (m4 < mo) "No"
+    else "Yes"
+
+  println(answer)
+}
