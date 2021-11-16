d0b28879f4d831fe96b3c7f2d0adc2a558edc4f1
ryskit <ryosukekida.71@gmail.com>
2021-11-17 00:11:14 +0900

    done abc224 - A

--- abc/224/A.scala
+++ abc/224/A.scala
@@ -1,3 +1,11 @@
-package 224 object A {
+package abc224A
 
+object Main extends App {
+  val sc = new java.util.Scanner(System.in)
+  val s = sc.next
+  if (s.last == 'r') {
+    println("er")
+  } else {
+    println("ist")
+  }
 }
