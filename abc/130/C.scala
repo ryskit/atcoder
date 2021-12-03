5e5ce59694b53800bc003e1f031e900d5e3068ee
ryskit <ryosukekida.71@gmail.com>
2021-12-04 02:28:12 +0900

    fix abc130 - C

--- abc/130/C.scala
+++ abc/130/C.scala
@@ -4,6 +4,6 @@ object Main extends App {
   val sc = new java.util.Scanner(System.in)
   val w, h, x, y = sc.nextLong
   val ans1 = w.toDouble * h.toDouble / 2.0
-  val ans2 = if (w == x * 2 && h == y * 2) 1 else 1
+  val ans2 = if (w == x * 2 && h == y * 2) 1 else 0
   println(f"$ans1%.6f $ans2")
 }
