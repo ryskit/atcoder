0cf40dd06353bbb4a3fd682be87f4b539fbc6851
ryskit <ryosukekida.71@gmail.com>
2022-01-08 23:11:26 +0900

    done abc078 - C

--- abc/078/C.scala
+++ abc/078/C.scala
@@ -4,5 +4,5 @@ object Main extends App {
   val sc = new java.util.Scanner(System.in)
   val n, m = sc.nextLong
   val times = 1900L * m + 100L * (n - m)
-  println((1L << 1) * times)
+  println((1L << m) * times)
 }
